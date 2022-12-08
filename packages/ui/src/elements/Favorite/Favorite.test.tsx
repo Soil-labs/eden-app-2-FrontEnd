9530cd4f334e1609b455bcf0b19e9ab3fea9d399
with-heart <with.heart+git@pm.me>
2022-12-08 12:26:46 -0500

    add test for Favorite
    Co-authored-by: sbelka-1703 <sbelka93@gmail.com>

--- packages/ui/src/elements/Favorite/Favorite.test.tsx
+++ packages/ui/src/elements/Favorite/Favorite.test.tsx
@@ -1,18 +1,21 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { Favorite } from ".";
 
-describe("Favorite", () => {
-  it("renders without throwing", () => {
-    const { container } = render(
-      <Favorite
-        favorite={false}
-        onFavorite={() => {
-          console.log("update favorite");
-        }}
-      />
-    );
+test("when user clicks the favorite button to trigger the on favorite callback", async () => {
+  const user = userEvent.setup();
+  const onFavorite = jest.fn();
 
-    expect(container).toBeInTheDocument();
-  });
+  const { rerender } = render(<Favorite favorite onFavorite={onFavorite} />);
+
+  // find the button, click it, and validate that callback is called
+  await user.click(screen.getByRole("button"));
+  expect(onFavorite).toHaveBeenCalledTimes(1);
+
+  // enabled the `disabled` prop, click the button again, and validate that the
+  // callback isn't called again
+  rerender(<Favorite favorite onFavorite={onFavorite} disabled />);
+  await user.click(screen.getByRole("button"));
+  expect(onFavorite).toHaveBeenCalledTimes(1);
 });
