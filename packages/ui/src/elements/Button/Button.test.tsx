aaff74be33862ccc6a0e358264f92920747c69c7
with-heart <with.heart+git@pm.me>
2022-12-08 15:16:43 -0500

    add tests for Button

--- packages/ui/src/elements/Button/Button.test.tsx
+++ packages/ui/src/elements/Button/Button.test.tsx
@@ -1,11 +1,26 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { Button } from "./";
 
-describe("Button", () => {
-  it("renders without throwing", () => {
-    const { container } = render(<Button />);
+test("user can click the button", async () => {
+  const user = userEvent.setup();
+  const onClick = jest.fn();
 
-    expect(container).toBeInTheDocument();
-  });
+  // interactive by default
+  const { rerender } = render(<Button onClick={onClick}>Hello World</Button>);
+
+  await user.click(screen.getByRole("button", { name: /hello world/i }));
+
+  expect(onClick).toHaveBeenCalledTimes(1);
+
+  // re-render with `disabled`, no longer interactive
+  rerender(
+    <Button disabled onClick={onClick}>
+      Hello World
+    </Button>
+  );
+  await user.click(screen.getByRole("button", { name: /hello world/i }));
+
+  expect(onClick).toHaveBeenCalledTimes(1);
 });
