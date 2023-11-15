c397cf98d85efaebe42766d1904fc0a7b7479ca5
Miltiadis Saratzidis <miltossaratzidis1@gmail.com>
2023-11-14 21:45:57 -0500

    test

--- packages/ui/src/sections/Missing404Section/Missing404Section.tsx
+++ packages/ui/src/sections/Missing404Section/Missing404Section.tsx
@@ -6,13 +6,14 @@ export const Missing404Section = ({}: IMissing404SectionProps) => {
   const router = useRouter();
 
   return (
-    <div className="flex flex-col-reverse items-center justify-center gap-16 px-4 py-24 md:gap-28 md:py-20 md:px-44 lg:flex-row lg:px-24 lg:py-24">
+    <div className="flex flex-col-reverse items-center justify-center gap-16 px-4 py-24 md:gap-28 md:px-44 md:py-20 lg:flex-row lg:px-24 lg:py-24">
       <div className="relative w-full pb-12 lg:pb-0 xl:w-1/2 xl:pt-24">
         <div className="relative">
           <div className="absolute">
             <div className="">
               <h1 className="my-2 text-2xl font-bold text-gray-800">
-                Looks like you&apos;ve found the doorway to the great nothing
+                Looks like you&apos;ve found the doorway to the great nothing --
+                1
               </h1>
               <p className="my-2 text-gray-800">
                 Sorry about that! Please visit our hompage to get where you need
@@ -20,7 +21,7 @@ export const Missing404Section = ({}: IMissing404SectionProps) => {
               </p>
               <button
                 onClick={() => router.push("/")}
-                className="md my-2 rounded border bg-indigo-600 py-4 px-8 text-center text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50 sm:w-full lg:w-auto"
+                className="md my-2 rounded border bg-indigo-600 px-8 py-4 text-center text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50 sm:w-full lg:w-auto"
               >
                 Take me there!
               </button>
